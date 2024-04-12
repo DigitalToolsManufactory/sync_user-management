58af6bd03ac00f7b6081585a3e89d6ae93bec5f0
Johannes Schneider <johannes.schneider03@sap.com>
2024-04-12 10:19:22 +0200

    initial commit

---
+++ src/test/java/com/github/dtm/sync/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dtm.sync.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
